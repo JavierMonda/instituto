3802cd4305b700b742a7eeb9f0074ef7973ce78a
Javier Jiménez García <javier.raidpc@yahoo.es>
2016-07-31 18:43:13 +0100

    Modificación archivo mysql-login.php
    Se han modificado los datos de conexión a la Base de Datos para su uso
    público

--- php/mysql-login.php
+++ php/mysql-login.php
@@ -1,7 +1,7 @@
 <?php
 //Datos de conexión a la base de datos
-$hostname = 'localhost';
-$database = 'dbInstituto';
-$username = 'root';
-$password = 'B0quepas';
+$hostname = 'localhost'; // Servidor
+$database = 'dbName'; // Nombre de la base de datos
+$username = 'root'; // Usuario
+$password = 'pass'; // Contraseña
 ?>
