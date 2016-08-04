377016fb233132ce8a53fa904442e75de7065e62
Javier Jiménez <javier.raidpc@yahoo.es>
2016-08-04 11:46:10 +0100

    update scriptCracionDb.sql

--- php/mysql-login.php
+++
@@ -1,7 +0,0 @@
-<?php
-//Datos de conexión a la base de datos
-$hostname = 'localhost'; // Servidor
-$database = 'dbName'; // Nombre de la base de datos
-$username = 'root'; // Usuario
-$password = 'pass'; // Contraseña
-?>
