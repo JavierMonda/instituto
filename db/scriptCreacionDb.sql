5abfe3b4ea6ddd0284ba0d9fab941eb765262c7a
Javier Jiménez <javier.raidpc@yahoo.es>
2016-08-04 11:48:45 +0100

    update

--- db/scriptCreacionDb.sql
+++ db/scriptCreacionDb.sql
@@ -1,8 +1,8 @@
-CREATE DATABASE  IF NOT EXISTS `dbinstituto` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_spanish_ci */;
-USE `dbinstituto`;
+CREATE DATABASE  IF NOT EXISTS `dbInstituto` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_spanish_ci */;
+USE `dbInstituto`;
 -- MySQL dump 10.13  Distrib 5.7.9, for Win32 (AMD64)
 --
--- Host: localhost    Database: dbinstituto
+-- Host: localhost    Database: dbInstituto
 -- ------------------------------------------------------
 -- Server version	5.6.27-log
 
@@ -18,13 +18,13 @@ USE `dbinstituto`;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `tasignatura`
+-- Table structure for table `tAsignatura`
 --
 
-DROP TABLE IF EXISTS `tasignatura`;
+DROP TABLE IF EXISTS `tAsignatura`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `tasignatura` (
+CREATE TABLE `tAsignatura` (
   `nombreAsignatura` varchar(45) NOT NULL,
   `descripcionAsig` varchar(90) DEFAULT NULL,
   `numHoras` int(11) NOT NULL,
@@ -33,13 +33,13 @@ CREATE TABLE `tasignatura` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Table structure for table `tcapacitacion`
+-- Table structure for table `tCapacitacion`
 --
 
-DROP TABLE IF EXISTS `tcapacitacion`;
+DROP TABLE IF EXISTS `tCapacitacion`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `tcapacitacion` (
+CREATE TABLE `tCapacitacion` (
   `idCapacitacion` int(11) NOT NULL,
   `nombreCapacitacion` varchar(45) NOT NULL,
   `descripcion` varchar(90) DEFAULT NULL,
@@ -48,75 +48,75 @@ CREATE TABLE `tcapacitacion` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Table structure for table `tciclo`
+-- Table structure for table `tCiclo`
 --
 
-DROP TABLE IF EXISTS `tciclo`;
+DROP TABLE IF EXISTS `tCiclo`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `tciclo` (
+CREATE TABLE `tCiclo` (
   `nombreCiclo` varchar(45) NOT NULL,
   `grado` enum('Medio','Superior') NOT NULL,
-  `modalidad` varchar(45) NOT NULL,
+  `modalidad` enum('Presencial','Semi-presencial','A distacia') NOT NULL,
   PRIMARY KEY (`nombreCiclo`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Table structure for table `tcurso`
+-- Table structure for table `tCurso`
 --
 
-DROP TABLE IF EXISTS `tcurso`;
+DROP TABLE IF EXISTS `tCurso`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `tcurso` (
+CREATE TABLE `tCurso` (
   `nombreCurso` varchar(45) NOT NULL,
   PRIMARY KEY (`nombreCurso`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Table structure for table `tobtiene`
+-- Table structure for table `tObtiene`
 --
 
-DROP TABLE IF EXISTS `tobtiene`;
+DROP TABLE IF EXISTS `tObtiene`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `tobtiene` (
+CREATE TABLE `tObtiene` (
   `nombreCicloOb` varchar(45) NOT NULL,
   `idCapacitacionOb` int(11) NOT NULL,
   PRIMARY KEY (`nombreCicloOb`,`idCapacitacionOb`),
   KEY `idCapacitacionObfk_idx` (`idCapacitacionOb`),
-  CONSTRAINT `idCapacitacionObfk` FOREIGN KEY (`idCapacitacionOb`) REFERENCES `tcapacitacion` (`idCapacitacion`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `nombreCicloObfk` FOREIGN KEY (`nombreCicloOb`) REFERENCES `tciclo` (`nombreCiclo`) ON DELETE CASCADE ON UPDATE CASCADE
+  CONSTRAINT `idCapacitacionObfk` FOREIGN KEY (`idCapacitacionOb`) REFERENCES `tCapacitacion` (`idCapacitacion`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `nombreCicloObfk` FOREIGN KEY (`nombreCicloOb`) REFERENCES `tCiclo` (`nombreCiclo`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Table structure for table `tprepara`
+-- Table structure for table `tPrepara`
 --
 
-DROP TABLE IF EXISTS `tprepara`;
+DROP TABLE IF EXISTS `tPrepara`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `tprepara` (
+CREATE TABLE `tPrepara` (
   `nombreCicloPre` varchar(45) NOT NULL,
   `idSalidaProPre` int(11) NOT NULL,
   PRIMARY KEY (`nombreCicloPre`,`idSalidaProPre`),
   KEY `idSalidaProPrefk_idx` (`idSalidaProPre`),
-  CONSTRAINT `idSalidaProPrefk` FOREIGN KEY (`idSalidaProPre`) REFERENCES `tsalidaprofesional` (`idSalidaPro`) ON UPDATE CASCADE,
-  CONSTRAINT `nombreCicloPrefk` FOREIGN KEY (`nombreCicloPre`) REFERENCES `tciclo` (`nombreCiclo`) ON UPDATE CASCADE
+  CONSTRAINT `idSalidaProPrefk` FOREIGN KEY (`idSalidaProPre`) REFERENCES `tSalidaprofesional` (`idSalidaPro`) ON UPDATE CASCADE,
+  CONSTRAINT `nombreCicloPrefk` FOREIGN KEY (`nombreCicloPre`) REFERENCES `tCiclo` (`nombreCiclo`) ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Table structure for table `tsalidaprofesional`
+-- Table structure for table `tSalidaProfesional`
 --
 
-DROP TABLE IF EXISTS `tsalidaprofesional`;
+DROP TABLE IF EXISTS `tSalidaProfesional`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `tsalidaprofesional` (
+CREATE TABLE `tSalidaProfesional` (
   `idSalidaPro` int(11) NOT NULL,
   `nombreSalidaPro` varchar(45) NOT NULL,
   PRIMARY KEY (`idSalidaPro`)
@@ -124,49 +124,49 @@ CREATE TABLE `tsalidaprofesional` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Table structure for table `ttiene`
+-- Table structure for table `tTiene`
 --
 
-DROP TABLE IF EXISTS `ttiene`;
+DROP TABLE IF EXISTS `tTiene`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `ttiene` (
+CREATE TABLE `tTiene` (
   `nombreCicloT` varchar(45) NOT NULL,
   `nombreCursoT` varchar(45) NOT NULL,
   `nombreAsignaturaT` varchar(45) NOT NULL,
   PRIMARY KEY (`nombreCicloT`,`nombreCursoT`,`nombreAsignaturaT`),
   KEY `nombreCursoTfk_idx` (`nombreCursoT`),
   KEY `nombreAsignaturaTfk_idx` (`nombreAsignaturaT`),
-  CONSTRAINT `nombreAsignaturaTfk` FOREIGN KEY (`nombreAsignaturaT`) REFERENCES `tasignatura` (`nombreAsignatura`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `nombreCicloTfk` FOREIGN KEY (`nombreCicloT`) REFERENCES `tciclo` (`nombreCiclo`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `nombreCursoTfk` FOREIGN KEY (`nombreCursoT`) REFERENCES `tcurso` (`nombreCurso`) ON DELETE CASCADE ON UPDATE CASCADE
+  CONSTRAINT `nombreAsignaturaTfk` FOREIGN KEY (`nombreAsignaturaT`) REFERENCES `tAsignatura` (`nombreAsignatura`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `nombreCicloTfk` FOREIGN KEY (`nombreCicloT`) REFERENCES `tCiclo` (`nombreCiclo`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `nombreCursoTfk` FOREIGN KEY (`nombreCursoT`) REFERENCES `tCurso` (`nombreCurso`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Table structure for table `tunidadtrabajo`
+-- Table structure for table `tUnidadTrabajo`
 --
 
-DROP TABLE IF EXISTS `tunidadtrabajo`;
+DROP TABLE IF EXISTS `tUnidadTrabajo`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `tunidadtrabajo` (
+CREATE TABLE `tUnidadTrabajo` (
   `nombreUT` varchar(45) NOT NULL,
   `descripcionUT` varchar(45) DEFAULT NULL,
   `apuntes` varchar(90) DEFAULT NULL,
   `nombreAsignaturaUT` varchar(45) NOT NULL,
   PRIMARY KEY (`nombreUT`),
   KEY `nombreAsignaturaUTfk_idx` (`nombreAsignaturaUT`),
-  CONSTRAINT `nombreAsignaturaUTfk` FOREIGN KEY (`nombreAsignaturaUT`) REFERENCES `tasignatura` (`nombreAsignatura`) ON DELETE CASCADE ON UPDATE CASCADE
+  CONSTRAINT `nombreAsignaturaUTfk` FOREIGN KEY (`nombreAsignaturaUT`) REFERENCES `tAsignatura` (`nombreAsignatura`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping events for database 'dbinstituto'
+-- Dumping events for database 'dbInstituto'
 --
 
 --
--- Dumping routines for database 'dbinstituto'
+-- Dumping routines for database 'dbInstituto'
 --
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
